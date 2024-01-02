f92778a0df4dc8b7674e43f7451ea56d28347bce
ramazan3ren <ramazan3ren@gmail.com>
2024-01-02 18:24:44 +0300

    logo error fixed3

--- src/hooks/useDarkSide.jsx
+++ src/hooks/useDarkSide.jsx
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 
 export default function useDarkSide() {
- 
   const [theme, setTheme] = useState(localStorage.theme);
   const colorTheme = theme === "dark" ? "light" : "dark";
 
   useEffect(() => {
     const body = window.document.documentElement;
     body.classList.add(colorTheme);
-    body.classList.remove(colorTheme);
     body.classList.add(theme);
+    body.classList.remove(colorTheme);
+
     localStorage.setItem("theme", theme);
+
     document.body.className = localStorage.theme;
   }, [theme, colorTheme]);
 
