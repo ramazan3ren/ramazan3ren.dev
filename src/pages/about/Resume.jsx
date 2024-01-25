ee9cb8b333441c25abe15c07fd6356c13dd557e1
ramazan3ren <ramazan3ren@gmail.com>
2024-01-25 15:58:02 +0300

    Resume page edited and resumeItems added

--- src/pages/about/Resume.jsx
+++ src/pages/about/Resume.jsx
@@ -1,10 +1,36 @@
 import React from "react";
-
+import LaptopWindowsRoundedIcon from "@mui/icons-material/LaptopWindowsRounded";
+import MonitorRoundedIcon from "@mui/icons-material/MonitorRounded";
+import SupervisorAccountRoundedIcon from "@mui/icons-material/SupervisorAccountRounded";
+import { ResumeItems } from "./ResumeItems";
 export const Resume = () => {
+  const resumeObject = [
+    {
+      id: "1",
+      Icon: () => <LaptopWindowsRoundedIcon sx={{ fontSize: 40 }} />,
+      title: "Responsive Tasarım",
+      value:
+        "Web sitesinin veya uygulamanın farklı cihazlarda ve ekran boyutlarında sorunsuz bir şekilde çalışmasını sağlar.",
+    },
+    {
+      id: "2",
+      Icon: () => <MonitorRoundedIcon sx={{ fontSize: 40 }} />,
+      title: "Yaratıcı Tasarım",
+      value:
+        "Yaratıcı tasarım, estetik ve işlevselliği birleştirerek etkileyici görsel tasarım oluşturulmasıdır.",
+    },
+    {
+      id: "3",
+      Icon: () => <SupervisorAccountRoundedIcon sx={{ fontSize: 40 }} />,
+      title: "Yönetim Paneli",
+      value:
+        "Web uygulamasının arkasındaki içeriği yönetmek için kullanılan arayüzdür.",
+    },
+  ];
   return (
     <>
-      <div className="w-full bg-white dark:bg-darkThemeColor py-24 flex flex-column items-center justify-center lg:py-36">
-        <div className="lg:px-80 px-10">
+      <div className="w-full bg-white dark:bg-darkThemeColor py-24 flex flex-col items-center justify-center lg:py-36">
+        <div className="1080p:px-64 lg:px-40 px-10 mb-10">
           <h2 className="uppercase font-josefin mb-5 text-lg lg:text-3xl text-primaryColor">
             İşinize Yeni ve Yaratıcı Bir Başlangıç Yapabilirim!
           </h2>
@@ -16,6 +42,17 @@ export const Resume = () => {
             kendimi sürekli olarak yenmeyi hedefliyorum. Bana her türlü soru,
             öneri veya işbirliği için ulaşmaktan çekinmeyin.
           </p>
+        </div>
+        <div className="grid grid-flow-row lg:grid-flow-col w-full 1080p:px-60  px-10 ">
+          {resumeObject.map((resumeItem) => {
+            return (
+              <ResumeItems
+                key={resumeItem.id} 
+                items={resumeItem}
+                Icon={resumeItem.Icon}
+              />
+            );
+          })}
         </div>
       </div>
     </>
---
+++ src/pages/about/ResumeItems.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const ResumeItems = ({ Icon, items }) => {
+  return (
+    <>
+      <div className="flex flex-row text-black dark:text-white mr-5 lg:px-4 mb-6">
+        <span className="mr-5 ">
+          <Icon />
+        </span>
+        <div>
+          <h3 className="1080p:text-2xl text-primaryColor font-josefin mb-1">
+            {items.title}
+          </h3>
+          <p className="text-sm">{items.value}</p>
+        </div>
+      </div>
+    </>
+  );
+};
